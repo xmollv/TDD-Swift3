63db78d2317dc738be06d589298de74cae1a8f49
Xavi Moll <xmollv@gmail.com>
2017-02-07 16:40:44 +0100

    Refactored the tests to be easier to read

--- FirstDemo/FirstDemoTests/FirstDemoTests.swift
+++ FirstDemo/FirstDemoTests/FirstDemoTests.swift
@@ -30,16 +30,19 @@ class FirstDemoTests: XCTestCase {
         XCTAssertEqual(numberOfVowels, 3, "should find 3 vowels in Dominik", line: 24)
     }
     
-    func test_MakeGeadline_ReturnsStringWithEachWordStartCapital() {
-        let string = "this is A test headline"
-        let headline = viewController.makeHeadline(from: string)
-        XCTAssertEqual(headline, "This Is A Test Headline")
+    func test_MakeHeadline_ReturnsStringWithEachWordStartCapital() {
+        let input = "this is A test headline"
+        let expectedOutput = "This Is A Test Headline"
+        
+        let headline = viewController.makeHeadline(from: input)
+        XCTAssertEqual(headline, expectedOutput)
     }
     
     func test_MakeHeadline_ReturnsStringWithEachWordStartCapital2() {
-        let string = "Here is another Example"
-        let headline = viewController.makeHeadline(from: string)
-        XCTAssertEqual(headline, "Here Is Another Example")
+        let input = "Here is another Example"
+        let expectedOutput = "Here Is Another Example"
+        let headline = viewController.makeHeadline(from: input)
+        XCTAssertEqual(headline, expectedOutput)
     }
     
 }
