5e6769f66bcedf86210d2c1b9962217e6b5921b2
Xavi Moll <xmollv@gmail.com>
2017-02-07 13:40:49 +0100

    Added the base project

---
+++ FirstDemo/FirstDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstDemo
+//
+//  Created by Xavi Moll on 07/02/17.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
