43dc6d26e5dfda0f705dad53779cbd50e5f0048a
Xavi Moll <xmollv@gmail.com>
2017-05-14 17:36:32 +0200

    Created the custom tableViewCell

---
+++ ToDo/ToDo/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  ToDo
+//
+//  Created by Xavi Moll on 14/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    
+    
+}
--- ToDo/ToDoTests/ItemListDataProviderTests.swift
+++ ToDo/ToDoTests/ItemListDataProviderTests.swift
@@ -55,7 +55,14 @@ class ItemListDataProviderTests: XCTestCase {
         tableView.reloadData()
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+    func test_CellForRow_ReturnsItemCell() {
+        sut.itemManager?.add(ToDoItem(title: "Foo"))
+        tableView.reloadData()
         
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        XCTAssertTrue(cell is ItemCell)
     }
     
 }
