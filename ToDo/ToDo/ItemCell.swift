dfd25baa3b3cfdc3e5aeeb0d68a5e9fa2b1fbabc
Xavi Moll <xmollv@gmail.com>
2017-05-14 23:11:59 +0200

    Tested that when an item is checked, the title has an attributed string with an strike through style

--- ToDo/ToDo/ItemCell.swift
+++ ToDo/ToDo/ItemCell.swift
@@ -20,13 +20,21 @@ class ItemCell: UITableViewCell {
         return dateFormatter
     }()
     
-    func configCell(with item: ToDoItem) {
-        titleLabel.text = item.title
-        locationLabel.text = item.location?.name
-        
-        if let timestamp = item.timestamp {
-            let date = Date(timeIntervalSince1970: timestamp)
-            dateLabel.text = dateFormatter.string(from: date)
+    func configCell(with item: ToDoItem, checked: Bool = false) {
+        if checked {
+            let attributedString = NSAttributedString(string: item.title, attributes: [NSStrikethroughStyleAttributeName:NSUnderlineStyle.styleSingle.rawValue])
+            
+            titleLabel.attributedText = attributedString
+            locationLabel.text = nil
+            dateLabel.text = nil
+        } else {
+            titleLabel.text = item.title
+            locationLabel.text = item.location?.name
+            
+            if let timestamp = item.timestamp {
+                let date = Date(timeIntervalSince1970: timestamp)
+                dateLabel.text = dateFormatter.string(from: date)
+            }
         }
     }
     
