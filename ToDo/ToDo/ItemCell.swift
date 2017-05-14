212b0e95b7f06cf0957c8daa5cd04dd8220ea0a7
Xavi Moll <xmollv@gmail.com>
2017-05-14 22:56:28 +0200

    Refactored the previous tests to remove the duplicated code and added the dateLabel

--- ToDo/ToDo/ItemCell.swift
+++ ToDo/ToDo/ItemCell.swift
@@ -12,6 +12,7 @@ class ItemCell: UITableViewCell {
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
     
     func configCell(with item: ToDoItem) {
         
