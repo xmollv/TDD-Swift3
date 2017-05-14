d954f3947f96ca6c508b8801ade04d2d9e5679c5
Xavi Moll <xmollv@gmail.com>
2017-05-14 16:31:20 +0200

    Created the view controller that will handle the list of items. I've also created a test to check if the tableView is present after viewDidLoad

---
+++ ToDo/ToDo/ItemListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemListViewController.swift
+//  ToDo
+//
+//  Created by Xavi Moll on 14/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+    
+    var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        tableView = UITableView()
+    }
+}
---
+++ ToDo/ToDoTests/ItemListViewControllerTest.swift
@@ -0,0 +1,30 @@
+//
+//  ItemListViewControllerTest.swift
+//  ToDo
+//
+//  Created by Xavi Moll on 14/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListViewControllerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_TableViewIsNotNilAfterViewDidLoad() {
+        let sut = ItemListViewController()
+        _ = sut.view
+        XCTAssertNotNil(sut.tableView)
+    }
+    
+}
