f55e9d3039cdefa010609bff681e030390557a76
Xavi Moll <xmollv@gmail.com>
2017-05-22 22:42:21 +0200

    Added the missing labels and added a test to check that the values that we are setting are the ones expected

--- ToDo/ToDo/DetailViewController.swift
+++ ToDo/ToDo/DetailViewController.swift
@@ -12,6 +12,39 @@ import MapKit
 class DetailViewController: UIViewController {
     
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
     @IBOutlet var mapView: MKMapView!
-
+    
+    var itemInfo: (ItemManager, Int)?
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.0.item(at: itemInfo.1)
+        
+        titleLabel.text = item.title
+        locationLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        
+        if let timestamp = item.timestamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate,
+                                                            100, 100)
+            mapView.region = region 
+        }
+    }
 }
--- ToDo/ToDoTests/DetailViewControllerTests.swift
+++ ToDo/ToDoTests/DetailViewControllerTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import ToDo
+import CoreLocation
 
 class DetailViewControllerTests: XCTestCase {
     
@@ -31,6 +32,28 @@ class DetailViewControllerTests: XCTestCase {
     
     func test_HasMapView() {
         XCTAssertNotNil(sut.mapView)
+    }
+    
+    func test_SettingItemInfo_SetsTextsToLabels() {
+        let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
+        let location = Location(name: "Foo", coordinate: coordinate)
+        let item = ToDoItem(title: "Bar", itemDescription: "Baz", timestamp: 1456150025, location: location)
+        let itemManager = ItemManager()
+        itemManager.add(item)
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+        XCTAssertEqual(sut.titleLabel.text, "Bar")
+        XCTAssertEqual(sut.dateLabel.text, "02/22/2016")
+        XCTAssertEqual(sut.locationLabel.text, "Foo")
+        XCTAssertEqual(sut.descriptionLabel.text, "Baz")
+        XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.latitude,
+                                   coordinate.latitude,
+                                   accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(sut.mapView.centerCoordinate.longitude,
+            coordinate.longitude,
+            accuracy: 0.001)
     }
     
 }
