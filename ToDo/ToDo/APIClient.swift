cc38112a3cfcdaa33e89a78b3780295b5df6755e
Xavi Moll <xmollv@gmail.com>
2017-05-23 22:30:46 +0200

    Added a test for the login

---
+++ ToDo/ToDo/APIClient.swift
@@ -0,0 +1,26 @@
+//
+//  APIClient.swift
+//  ToDo
+//
+//  Created by Xavi Moll on 23/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import Foundation
+
+protocol SessionProtocol {
+    func dataTask( with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+class APIClient {
+    lazy var session: SessionProtocol = URLSession.shared
+    
+    func loginUser(withName username: String, password: String, completion: @escaping (Token?, Error?) -> Void) {
+        guard let url = URL(string: "https://awesometodos.com") else { fatalError() }
+        session.dataTask(with: url) { (data, response, error) in
+    
+        }
+    }
+}
+
+extension URLSession: SessionProtocol {}
---
+++ ToDo/ToDoTests/APIClientTests.swift
@@ -0,0 +1,45 @@
+//
+//  APIClientTests.swift
+//  ToDo
+//
+//  Created by Xavi Moll on 23/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class APIClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_Login_UsesExpectedURL() {
+        let sut = APIClient()
+        let mockURLSession = MockURLSession()
+        sut.session = mockURLSession
+        let completion = { (token: Token?, error: Error?) in }
+        sut.loginUser(withName:"dasdom", password: "1234", completion: completion)
+        guard let url = mockURLSession.url else { XCTFail(); return }
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        XCTAssertEqual(urlComponents?.host, "awesometodos.com")
+    }
+    
+}
+
+extension APIClientTests {
+    class MockURLSession: SessionProtocol {
+        var url: URL?
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+                self.url = url
+                return URLSession.shared.dataTask(with: url)
+        } 
+    } 
+}
